e58382312eedf353353073178a9701f816732540
Abelardo J. Marquez <ajmarquezg@gmail.com>
2015-09-30 22:37:34 -0430

    Added 2 new tests

--- reactron.rb
+++ reactron.rb
@@ -2,7 +2,14 @@
 
 class ReactionTracker
 
-  def initialize array
-    @array = array
+    attr_accessor :array
+
+  def initialize react_array
+    @array = react_array
   end
+
+  def matched (dummy_array)
+    @array.sort == dummy_array.sort
+  end
+
 end
--- spec/reactron_spec.rb
+++ spec/reactron_spec.rb
@@ -2,17 +2,35 @@ require 'spec_helper'
 
 describe ReactionTracker do
 
-  before :each do
-    @reaction_list = ReactionTracker.new [':imp:',':+1:']
+  before :example do
+    @reaction_list = ReactionTracker.new ['imp','+1']
   end
 
   describe "#new" do
-    it "creates an array of reactions" do
-      expect(@reaction_list).to be_instance_of ReactionTracker
+
+    it "Creates an array of reactions" do
+      expect(@reaction_list.array).to be_an Array
     end
 
     it "Should not be allowed" do
       expect(lambda{ReactionTracker.new("array", "array2")}).to raise_exception ArgumentError
+    end
+  end
+
+  describe ".matched" do
+
+    describe "Takes the ReactionTracker array and compares it with another" do
+
+      it "Returns true when arrays match" do
+        @dummyArray = ['imp','+1']
+        expect(@reaction_list.matched(@dummyArray)).to be_truthy
+
+      end
+
+      it "Returns false when arrays don't match" do
+        @dummyArray = ['imp']
+        expect(@reaction_list.matched(@dummyArray)).to be_falsey
+      end
     end
   end
 
